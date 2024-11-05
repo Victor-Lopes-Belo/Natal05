5889450dec710117b821e2752e69f1a4e9b8097f
Victor Lopes <99993119+Victor-Lopes-Belo@users.noreply.github.com>
2024-11-05 18:24:17 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function createSnowflake() {
+    const snowflake = document.createElement("div");
+    snowflake.classList.add("snowflake");
+    snowflake.textContent = "❄";
+    snowflake.style.left = Math.random() * window.innerWidth + "px";
+    snowflake.style.animationDuration = Math.random() * 3 + 2 + "s";
+    document.body.appendChild(snowflake);
+    setTimeout(() => snowflake.remove(), 5000);
+}
+setInterval(createSnowflake, 300);
